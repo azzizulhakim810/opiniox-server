716768659d84f33be8086d0f4f1ed7c3ed6b616f
azzizulhakim810 <azzizul.hakim202@gmail.com>
2023-12-06 12:49:52 +0600

    add all the codes

--- index.js
+++ index.js
@@ -28,6 +28,9 @@ async function run() {
     const postCollection = client.db("opinioXDB").collection("allposts");
     const commentCollection = client.db("opinioXDB").collection("allComments");
     const userCollection = client.db("opinioXDB").collection("allUsers");
+    const tagCollection = client.db("opinioXDB").collection("allTags");
+    const announcementCollection = client.db("opinioXDB").collection("allAnnouncements");
+    const reportCollection = client.db("opinioXDB").collection("allReports");
 
     //Count Comments
     app.get("/commentsCount", async (req, res) => {
@@ -35,13 +38,32 @@ async function run() {
       res.send({ result });
     });
 
+    // All tags 
+    app.get("/allTags", async (req, res) => {
+      const result = await tagCollection.find().toArray();
+      res.send(result);
+    });
+
+    // is admin 
+    app.get('/checkAdmin', async(req, res) => {
+      const email = req.query?.email;
+      console.log(email);
+      let query = {};
+      if(req.query?.email){
+        query = {email: req.query.email};
+        
+      }
+      const result = await userCollection.findOne(query);
+       res.send(result);
+    })
+
     // Load all posts with pagination
     app.get("/posts", async (req, res) => {
       const page = parseInt(req.query.page);
       const size = parseInt(req.query.size);
       const sortSystem = req.query.sortSystem || "popular";
-      const searchText = req.query.searchText || "";
-      console.log(searchText);
+      // const searchText = req.query?.searchText || 'undefined';
+      // console.log(searchText);
       // Validate that page and size are valid integers, defaulting to 0 and 10 if not provided
       const validPage = isNaN(page) ? 0 : Math.max(0, page);
       const validSize = isNaN(size) ? 10 : Math.max(1, size); // Adjust the minimum size as needed
@@ -61,11 +83,11 @@ async function run() {
             voteDifference: { $subtract: ["$upVote", "$downVote"] },
           },
         },
-        {
+        /* {
           $match: {
             tags: { $regex: searchText, $options: 'i'},
           }
-        },
+        }, */
         {
           $sort: sortQuery,
         },
@@ -92,6 +114,7 @@ async function run() {
     });
 
 
+    // get single post 
     app.get("/posts/single/:id", async (req, res) => {
       const id = req.params.id;
       // console.log(id);
@@ -106,6 +129,12 @@ async function run() {
       res.send({ count });
     });
 
+    // Get all User
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
     // Get a User
     app.get("/user", async (req, res) => {
       let query = {};
@@ -116,6 +145,18 @@ async function run() {
       const result = await userCollection.findOne(query);
       res.send(result);
     });
+
+     // Get all Announcements
+     app.get("/allAnnouncements", async (req, res) => {
+      const result = await announcementCollection.find().toArray();
+      res.send(result);
+    });
+
+       // Count announcements
+       app.get("/announcementCount", async (req, res) => {
+        const count = await announcementCollection.estimatedDocumentCount();
+        res.send({ count });
+      });
 
     // filter post by query parameter
     app.get("/recentPost", async (req, res) => {
@@ -160,6 +201,12 @@ async function run() {
       res.send(result);
     })
 
+        // get all reports 
+        app.get('/reports', async(req, res) => {
+           const result = await reportCollection.find().toArray();
+           res.send(result);
+         })
+
     // Post new post
     app.post("/posts", async (req, res) => {
       const newPost = req.body;
@@ -183,6 +230,58 @@ async function run() {
       const result = await userCollection.insertOne(user);
       res.send(result);
     });
+
+    // Add an announcement
+    app.post("/announcement", async (req, res) => {
+      const announcement = req.body;
+      // console.log(comment);
+      const result = await announcementCollection.insertOne(announcement);
+      res.send(result);
+    });
+    
+    // Add an announcement
+    app.post("/report", async (req, res) => {
+      const report = req.body;
+      // console.log(comment);
+      const result = await reportCollection.insertOne(report);
+      res.send(result);
+    });
+
+    // Comment update
+    app.patch("/updateComment/:id", async (req, res) => {
+      const id = req.params.id;
+      // console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const getPreviousComment = req.body;
+      // console.log(getPreviousComment);
+      const updateComment = {
+        $set: {
+          commentsCount: getPreviousComment.commentsCount + 1,
+        },
+      };
+      const result = await postCollection.updateOne(filter, updateComment);
+      res.send(result);
+    });
+
+    // Admin update
+    app.patch("/makeAdmin/:name", async (req, res) => {
+      const name = req.params.name;
+      console.log(name);
+      const filter = { name: name };
+      const getRole = req.body?.role;
+      // console.log(getRole);
+      if(getRole === 'user') {
+        const updateRole = {
+          $set: {
+            role: 'admin'
+          }
+        };
+        const result = await userCollection.updateOne(filter, updateRole);
+        res.send(result);
+      }
+      
+    });
+
 
     // Upvote update
     app.patch("/updateUpVote/:id", async (req, res) => {
@@ -222,6 +321,16 @@ async function run() {
       // console.log(id);
       const query = {_id: new ObjectId(id)};
       const result = await postCollection.deleteOne(query);
+      res.send(result);
+    })
+
+    // Delete Report 
+    app.delete('/report/delete/:id', async(req, res) => {
+      // const id = req.query.id;
+      const id = req.params.id;
+      // console.log(id);
+      const query = {_id: new ObjectId(id)};
+      const result = await reportCollection.deleteOne(query);
       res.send(result);
     })
 
